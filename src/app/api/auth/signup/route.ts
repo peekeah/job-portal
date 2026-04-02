7c3a526d891068515f7645bba58e614a5fd851a7
Pranay Kothari <pranay.kothari109@gmail.com>
2026-04-02 20:17:01 +0530

    update email validations in signup

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -8,7 +8,7 @@ import { UserType } from '@prisma/client';
 import { companySchema } from '@/lib/schema';
 
 const payloadSchema = z.object({
-  email: z.email().min(5).max(25),
+  email: z.email().min(5).max(50),
   password: z.string().min(5).max(20),
   user_type: z.enum(UserType).optional(),
 });
